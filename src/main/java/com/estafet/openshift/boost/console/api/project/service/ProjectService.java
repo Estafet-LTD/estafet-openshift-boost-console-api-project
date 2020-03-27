b6730fea16c504e2047662f9b60bce32e16825a7
mike2906 <mike.ruse90@gmail.com>
2020-03-27 12:10:39 +0200

    Changing back to pass username in path

--- src/main/java/com/estafet/openshift/boost/console/api/project/service/ProjectService.java
+++ src/main/java/com/estafet/openshift/boost/console/api/project/service/ProjectService.java
@@ -65,7 +65,7 @@ public class ProjectService {
 	public String createProject(Project project) {
 		User user = new User();
 		user.setName(project.getOwner());
-		user.setUid((restTemplate.getForObject(ENV.USER_SERVICE_API + "/user/name", User.class, user).getUid()));
+		user.setUid((restTemplate.getForObject(ENV.USER_SERVICE_API + "/user/name/" + project.getOwner() + "/", User.class).getUid()));
 		String uid = user.getUid();
 		System.out.println("User name: " + project.getOwner() + ", UID: " + uid);
 		client.executeCreateEnviromentPipeline(project, uid);
